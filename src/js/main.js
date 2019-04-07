6155e53971234cf47465eb0ea61fce3e431a2704
Egor Eremin <egor_eremin@mail.ru>
2019-04-07 22:47:16 +0800

    full header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import './vendor';
+$(document).ready(function () {
+
+	(function initStockSlider() {
+		$('.js-stock-slider').slick({
+			fade: false,
+			arrows: false,
+			dots: true,
+		});
+	})();
+
+});
+
+function media(mediaQueryString, action){
+	'use strict';
+	var handleMatchMedia = function (mediaQuery) {
+		if (mediaQuery.matches) { //Попадает в запроc
+			if (action  && typeof(action) === 'function') {
+				action();
+			}
+		}
+	};
+	var mql = window.matchMedia(mediaQueryString); //стандартный медиазапрос для смены режима просмотра
+	handleMatchMedia(mql);
+	mql.addListener(handleMatchMedia);
+}
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'slick-carousel';
 
 svg4everybody();
 
